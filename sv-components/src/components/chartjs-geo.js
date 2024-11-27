658ebb23150582cf0999d35064b93cfb26bf4f3a
Tomas Kulhanek <tmkulhanek@gmail.com>
2024-11-27 16:07:27 +0000

    UPDATE geo with description

--- sv-components/src/components/chartjs-geo.js
+++ sv-components/src/components/chartjs-geo.js
@@ -1,13 +1,14 @@
 import { Chart, registerables } from "chart.js/auto";
 import { bindable } from 'aurelia';
 import { topojson, ChoroplethController, GeoFeature, ProjectionScale, ColorScale } from "chartjs-chart-geo";
-import { whereNumeric } from 'iso-3166-1'
+import { whereNumeric,whereAlpha2 } from 'iso-3166-1'
 // 
 @customElement('sv-chartjs-geo')
 export class ChartjsGeo {
     @bindable datasrc = 'g/National_Initiatives_Figure_Data.csv';
     @bindable countriessrc = 'https://unpkg.com/world-atlas/countries-50m.json'
     geochart;
+    countryDataDesc = {};
     // Helper function to parse CSV
     parseCSV(csv) {
         const rows = csv.split('\n').map((row) => row.split(','));
@@ -19,32 +20,49 @@ export class ChartjsGeo {
             }, {})
         );
     }
+    
+    showSelected(selectedid){
+        console.log('showselected',selectedid)
+        this.selected = this.countryDataDesc[selectedid];
+        console.log(this.selected)
+    }
 
     attached() {
+        
         Chart.register(ChoroplethController, GeoFeature, ProjectionScale, ColorScale, ...registerables);
         Promise.all([
             fetch(this.countriessrc).then((res) => res.json()),
             fetch(this.datasrc).then((res) => res.text()), // Fetch the CSV file
         ]).then(([topoData, csvData]) => {
+            let that = this;
             const countries = topojson.feature(topoData, topoData.objects.countries).features;
             // Parse the CSV
             const parsedCSV = this.parseCSV(csvData);
             // Create a lookup for ISO country codes to project status (number)
             let countryDataMap = {}
+            this.countryDataDesc = {}
             for (let row of parsedCSV) {
-                countryDataMap[row['ISO 3166-1 alpha-2 Code']]=row['project status (number)']
+                let countryid = whereAlpha2(row['ISO 3166-1 alpha-2 Code']);
+                if (countryid) {
+                    countryDataMap[countryid.numeric]=row['project status (number)'];
+                    this.countryDataDesc[countryid.numeric]=row;
+                }
             }
             let myData = []
             for (let country of countries) {
-                let isocountry = whereNumeric(country.id)
+                //let isocountry = whereNumeric(country.id)
                 let myValue = 0;
-                if (isocountry) {
-                  let isocountryalpha2 = isocountry.alpha2
-                  let countryvalue = countryDataMap[isocountryalpha2]
-                  myValue = parseFloat(countryvalue);
-                } else {
-                    console.warn('cannot find alpha code for country with id'+ country.id+ ' name:' + country.properties.name);
-                }
+                //if (isocountry) {
+                  //let isocountryalpha2 = isocountry.alpha2
+                  //let countryvalue = countryDataMap[isocountryalpha2]
+                  //myValue = parseFloat(countryvalue);
+                //} else {
+                    //console.warn('cannot find alpha code for country with id'+ country.id+ ' name:' + country.properties.name);
+                //}
+                let rawValue = countryDataMap[country.id];
+                if (rawValue) {
+                    myValue = parseFloat(rawValue)
+                } else console.warn('no data for country with id'+ country.id+ ' name:' + country.properties.name);
                 myData.push({
                     feature: country,
                     value: myValue
@@ -52,10 +70,11 @@ export class ChartjsGeo {
             }
 
             //console.log('countries',countries);
+            
             const chart = new Chart(this.geochart.getContext("2d"), {
                 type: 'choropleth',
                 data: {
-                    labels: countries.map((d) => d.properties.name),
+                    labels: countries.map((d) => d.id +':'+d.properties.name),
                     datasets: [{
                         label: 'Countries',
                         //data: countries.map((d) => ({ feature: d, value: Math.random() })),
@@ -85,7 +104,13 @@ export class ChartjsGeo {
                                 align: 'right',
                             }
                         }
-                    }
+                    },
+                    onClick: (evt, elems) => {
+                        let myitem = elems.map((elem) => elem.element.feature.id)
+                        console.log('selected item + this + that', myitem,this,that);
+                        that.showSelected(myitem[0])
+
+                    },
                 }
             });
         });
